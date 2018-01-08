7c522d5a4a440ee3b2f88e987370104a32598dd6
_asdx_ <xasdx@tuta.io>
2018-01-08 18:03:22 +0000

    Fix failing tests due to misnamed modules

--- test/test.js
+++ test/test.js
@@ -7,7 +7,10 @@ test("imports user-defined modules", t => {
   t.is(m.behavior(), "hey")
 })
 
-test("imports external modules", t => importr("ext_ava"))
+test("imports external modules", t => {
+  let m = importr("ext_ava")
+  t.pass()
+})
 
 test("imports node modules", t => {
   let util = importr("node_util")
@@ -22,7 +25,7 @@ test("rejects non-existing user-defined modules", t => {
 })
 
 test("rejects ambiguous module names", t => {
-  let error = t.throws(() => importr("module_"))
+  let error = t.throws(() => importr("module-"))
   t.true(error.message.includes("found multiple matches for the given name"))
 })
 
@@ -32,10 +35,10 @@ test("rejects invalid module names", t => {
 })
 
 test("imports multiple modules", t => {
-  let m = importr(["node_util", "module_a", "module_b"])
+  let m = importr(["node_util", "module-a", "module-b"])
   t.true(m.node_util.isNull(null))
-  t.is(m.module_a.data, "hello")
-  t.is(m.module_b.behavior(), "hey")
+  t.is(m["module-a"].data, "hello")
+  t.is(m["module-b"].behavior(), "hey")
 })
 
 test("rejects empty lists when importing multiple modules", t => {
@@ -44,22 +47,22 @@ test("rejects empty lists when importing multiple modules", t => {
 })
 
 test("aliases module names", t => {
-  let m = importr(["node_util", "module_a"], ["util", "a"])
+  let m = importr(["node_util", "module-a"], ["util", "a"])
   t.true(m.util.isNull(null))
   t.is(m.a.data, "hello")
 })
 
 test("fails when module list and alias list are different in length", t => {
-  let error = t.throws(() => importr(["node_util", "module_a"], ["util"]))
+  let error = t.throws(() => importr(["node_util", "module-a"], ["util"]))
   t.true(error.message.includes("module list and alias list should be the same length"))
 })
 
 test("rejects non-string values as aliases", t => {
-  let error = t.throws(() => importr(["module_a"], [0]))
+  let error = t.throws(() => importr(["module-a"], [0]))
   t.true(error.message.includes("is not a valid alias"))
 })
 
 test("rejects empty strings as aliases", t => {
-  let error = t.throws(() => importr(["module_a"], [""]))
+  let error = t.throws(() => importr(["module-a"], [""]))
   t.true(error.message.includes("empty string is not a valid alias"))
 })
